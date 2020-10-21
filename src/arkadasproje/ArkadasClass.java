ee4343c8124e365e380000b87dc1a4744caf009d
Zfahruk <67062020+Zfahruk@users.noreply.github.com>
2020-10-22 01:06:20 +0200

    Update ArkadasClass.java
    commit

--- src/arkadasproje/ArkadasClass.java
+++ src/arkadasproje/ArkadasClass.java
@@ -3,6 +3,7 @@ package arkadasproje;
 public class ArkadasClass {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Selam arkadaslar");
 
 		System.out.println("hi  faruk branchtan");

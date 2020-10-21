16faa70d1a116f9b7429af2f5bddc30253176e1f
SelimTopbas <selimtopbas@gmail.com>
2020-10-21 22:42:32 +0200

    deneme3

--- src/arkadasproje/ArkadasClass.java
+++ src/arkadasproje/ArkadasClass.java
@@ -4,6 +4,17 @@ public class ArkadasClass {
 
 	public static void main(String[] args) {
 		System.out.println("Selam arkadaslar");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		System.out.println("Selimden deneme saat 22:50");
 		System.out.println("Bakalım hele, olacak mı?");
 		

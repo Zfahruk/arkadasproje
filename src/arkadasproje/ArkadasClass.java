715f35c94324ebd214b1d93ddb66b824577ee0e7
Zfahruk <67062020+Zfahruk@users.noreply.github.com>
2020-10-22 01:49:33 +0200

    Update ArkadasClass.java
    master 2

--- src/arkadasproje/ArkadasClass.java
+++ src/arkadasproje/ArkadasClass.java
@@ -9,6 +9,7 @@ public class ArkadasClass {
 		System.out.println("hi  faruk branchtan");
 		System.out.println("hi  master remote branchtan");
 		System.out.println("merhaba tekrar  local branch faruk tan");
+		System.out.println(" remote branch 2 master tan");
 
 
 

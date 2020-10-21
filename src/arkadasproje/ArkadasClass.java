e5d98fcfda10ae4650bd9328756d9c0971d13704
Zfahruk <synergyetkisi@gmail.com>
2020-10-22 00:37:46 +0200

    selamlar master

--- src/arkadasproje/ArkadasClass.java
+++ src/arkadasproje/ArkadasClass.java
@@ -4,13 +4,6 @@ public class ArkadasClass {
 
 	public static void main(String[] args) {
 		System.out.println("Selam arkadaslar");
-		System.out.println("Selimden deneme saat 22:50");
-		System.out.println("Bakalım hele, olacak mı?");		
-		System.out.println("Bu sefer olacak");
-		System.out.println("Selam arkadaslar");
-		System.out.println("Selam arkadaslar 2");
-
-
 		
 
 	}

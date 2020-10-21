1d3ee2c8d5497750522ddeba2f037d513e228cf8
SelimTopbas <selimtopbas@gmail.com>
2020-10-21 22:21:38 +0200

    selimden Faruk'a selamlar

--- src/arkadasproje/ArkadasClass.java
+++ src/arkadasproje/ArkadasClass.java
@@ -4,6 +4,10 @@ public class ArkadasClass {
 
 	public static void main(String[] args) {
 		System.out.println("Selam arkadaslar");
+		System.out.println("Selimden deneme saat 22:50");
+		System.out.println("Bakalım hele, olacak mı?");
+		
+		
 
 	}
 
